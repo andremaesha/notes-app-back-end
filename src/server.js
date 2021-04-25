217dec4145c4fc86742b6cf35ce22d2a9bd1ed9d
Andre Maesha <andremaesha@gmail.com>
2021-04-25 21:57:53 +0700

    intial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 8080,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
